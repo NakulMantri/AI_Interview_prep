9b4e058791fd7a9b1ae6696e31fb79417209b805
Nakulm10 <nakulm10@gmail.com>
2025-07-26 12:49:15 +0530

    igore  eslint and typescript errors  for now

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,10 @@
+'use client'
 import { cn } from '@/lib/utils';
+import { vapi } from '@/lib/vapi.sdk';
+import { VapiClient } from '@vapi-ai/server-sdk';
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 enum CallStatus {
     INACTIVE= 'INACTIVE',
@@ -9,17 +14,98 @@ enum CallStatus {
 
 }
 
+interface SavedMessage{
+  role:'user'|'system'|'assistant';
+  content:string
+}
+
+const Agent = ({ userName,userId,type }: AgentProps) => {
+
+  const router = useRouter();
+  const [isSpeaking,setiSpeaking] = useState(false);
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE)
 
 
-const Agent = ({ userName }: AgentProps) => {
-  const callStatus = CallStatus.INACTIVE;
-  const isSpeaking = true;
-  const messages = [
-    'What&apos;s your name?',
-    'My name is Amar, nice to meet you!'
-  ];
+ 
+  
+ const [messages,setMessages]=useState<SavedMessage[]>([])
 
-const lastMessage = messages[messages.length-1];
+
+ useEffect(()=>{
+  const onCallStart = ()=>setCallStatus(CallStatus.ACTIVE);
+  const onCallEnd =()=>setCallStatus(CallStatus.FINISHED);
+
+  const onMessage = (message:Message)=>{
+    if(message.type==='transcript'&&message.transcriptType==='final'){
+      const newMessage = {role:message.role,content:message.transcript}
+
+      setMessages((prev)=>[...prev,newMessage]);
+    }
+  }
+
+  const onSpeechStart = ()=>setiSpeaking(true);
+  const onSpeechEnd = ()=>setiSpeaking(false);
+  const onError = (error: Error) => console.error('VAPI Error:', error.message, error.stack); // ✅
+
+
+
+  vapi.on('call-start',onCallStart);
+  vapi.on('call-end',onCallEnd);
+  vapi.on('message',onMessage);
+  vapi.on('speech-start',onSpeechStart);
+  vapi.on('speech-end',onSpeechEnd);
+  vapi.on('error',onError);
+
+
+  return ()=>{
+      vapi.off('call-start',onCallStart);
+  vapi.off('call-end',onCallEnd);
+  vapi.off('message',onMessage);
+  vapi.off('speech-start',onSpeechStart);
+  vapi.off('speech-end',onSpeechEnd);
+  vapi.off('error',onError);
+  }
+
+ },[])
+
+useEffect(()=>{
+if(callStatus===CallStatus.FINISHED) router.push('/');
+
+
+},[messages,callStatus,type,userId])
+
+//Handling the start of the call//
+
+const handleCall = async()=>{
+  setCallStatus(CallStatus.CONNECTING);
+
+   await VapiClient.calls.startFromWorkflow({
+  workflowId: process.env.VAPI_WORKFLOW_ID!,
+  variables: {
+    username: userName,
+    userid: userId
+  }
+});
+
+   })
+
+ }
+
+
+
+
+
+
+const handleDisconnect = async()=>{
+  setCallStatus(CallStatus.FINISHED);
+
+  vapi.stop();
+}
+
+const latestMessage = messages[messages.length-1]?.content;
+
+
+const isCallInactiveOrFinished =callStatus=== CallStatus.INACTIVE||CallStatus.FINISHED;
 
   return (
     <>
@@ -58,8 +144,8 @@ const lastMessage = messages[messages.length-1];
 {messages.length>0&&(
     <div className='transcript-border'>
       <div className='transcript'>
-             <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>
-               {lastMessage}
+             <p key={latestMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>
+               {latestMessage}
              </p>
       </div>
     </div>
@@ -72,17 +158,17 @@ const lastMessage = messages[messages.length-1];
 
     <div className='w-full  flex justify-center'>
       {callStatus !== 'ACTIVE'?(
-        <button className='relative btn-call'>
+        <button className='relative btn-call' onClick={handleCall}>
            
-            <span className={cn('absolute animate-ping rounded-full opacity-75',callStatus!=='CONNECTING'&'hidden')} />
+            <span className={cn('absolute animate-ping rounded-full opacity-75',callStatus!=='CONNECTING'&&'hidden')} />
 
             <span>
-                 {callStatus ==='INACTIVE'|| callStatus==='FINISHED'?'Call':'. . .'}
+                 {isCallInactiveOrFinished?'Call':'. . .'}
             </span>
             
         </button>
       ):(
-        <button className='btn-disconnect'>
+        <button className='btn-disconnect' onClick={handleDisconnect}>
            End
         </button>
       )}
--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,4 +1,15 @@
-import Vapi from "@vapi-ai/web";
+// lib/vapi.ts
+import Vapi from "@vapi-ai/web"
+import { VapiClient } from "@vapi-ai/server-sdk"
 
-export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
+export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!)
+
+// ✅ Server SDK: for backend usage
+export const vapiClient = new VapiClient({
+  token: process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!, // secret token from .env
+})
+
 //ending with exclamation mark because we know it actually exists for typescript to believe the toke nactually exists
+
+
+
